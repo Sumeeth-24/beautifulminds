892631ef7144812c7cc5b99ddacea2820f2075b8
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2023-01-14 14:15:49 +0530

    first commit

--- client/src/api/ChatRequests.js
+++ client/src/api/ChatRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://beatifulminds-server.vercel.app/' });
 
 export const createChat = (data) => API.post('/chat/', data);
 
--- client/src/api/MessageRequests.js
+++ client/src/api/MessageRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://beatifulminds-server.vercel.app/' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 
